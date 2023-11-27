a4a2e3162f50aef87a652c58303b09d113863dfc
yo <alberto.montero@liferay.com>
2023-11-27 13:44:38 +0100

    Partial changes to process multiple breaking change report in one commit message

--- pretty-breaking-changes.py
+++ pretty-breaking-changes.py
@@ -1,30 +1,89 @@
 #! /usr/bin/python3
 
 import git
-import mistune
-import re
+# import mistune
+# import re
 
 repo_path = "/home/yo/src/liferay-portal"
 template_path = "/home/yo/projects/pretty-breaking-changes"
 
-markdown = mistune.create_markdown(renderer='ast')
+# markdown = mistune.create_markdown(renderer='ast')
+
+def get_end_of_block(lines, start_index, pattern):
+    if start_index >= len(lines):
+        return start_index
+    
+    end_of_block_index = start_index
+    
+    while end_of_block_index < len(lines):
+        if not lines[end_of_block_index].startswith(pattern):
+            end_of_block_index += 1
+        else:
+            break
+    
+    # print(end_of_block_index)
+    return end_of_block_index
+            
 
 def dissect_commit_message(raw_commit_message):
+    breaking_changes_info = []
+    
+    lines = raw_commit_message.split('\n')
+    
+    raw_jira_info_block_line_index = get_end_of_block(lines, 1, '# breaking')
+    
+    i = 0
+    while i < raw_jira_info_block_line_index:
+        if lines[i] != "":
+            jira_ticket, _, jira_ticket_title = lines[i].partition(' ')
+            break
+        i += 1
+    
+    previous_line_index = raw_jira_info_block_line_index
+    
+    while previous_line_index < len(lines):
+        # print(str(previous_line_index) + " " + str(len(lines)))
+        next_line_index = get_end_of_block(lines, previous_line_index, '----')
+        # print(next_line_index)
+        if next_line_index - previous_line_index > 1:
+            info = extract_breaking_change_info(lines[previous_line_index:next_line_index])
+            
+            if info:
+                breaking_changes_info.append(info)
+                breaking_changes_info[-1]['jira_ticket'] = jira_ticket
+                breaking_changes_info[-1]['jira_ticket_title'] = jira_ticket_title
+            else:
+                print("unprocessable")
+                print(lines[previous_line_index:next_line_index])
+                
+        previous_line_index = next_line_index + 1
+        
+    return breaking_changes_info
+    
+
+def extract_breaking_change_info(lines):
+    # print(lines)
     breaking_change_info = {}
     
     try:
-        aprcm = markdown(raw_commit_message)
-    
-        raw_jira_info = aprcm[0]['children'][0]['text']
-        breaking_change_info['jira_ticket'], _, breaking_change_info['jira_ticket_title'] = raw_jira_info.partition(' ')
-    
-        raw_what_header = aprcm[2]['children'][0]['text']
-        _, _, breaking_change_info['affected_file_path'] = raw_what_header.partition(' ')
-    
-        breaking_change_info['what_info'] = aprcm[3]['children'][0]['text']
-    
-        breaking_change_info['why_info'] = aprcm[5]['children'][0]['text']
-    except:
+        what_line_index = get_end_of_block(lines, 1, "## What")
+        raw_what_header = lines[what_line_index]
+        _, _, breaking_change_info['affected_file_path'] = raw_what_header.partition('## What ')
+        print(breaking_change_info['affected_file_path'])
+        
+        why_line_index = get_end_of_block(lines, what_line_index + 1, "## Why")
+
+        breaking_change_info['what_info'] = "\n".join(lines[what_line_index + 1:why_line_index])
+
+        alternatives_line_index = get_end_of_block(lines, why_line_index + 1, "## Alternatives")
+
+        breaking_change_info['why_info'] = "\n".join(lines[why_line_index + 1: alternatives_line_index])
+        
+        if len(lines) >= alternatives_line_index:
+            breaking_change_info['alternatives'] = "\n".join(lines[alternatives_line_index + 1: len(lines)])
+                    
+    except Exception as e:
+        print(e)
         # print(raw_commit_message)
         # print("--")
         return None
@@ -34,7 +93,11 @@ def dissect_commit_message(raw_commit_message):
 
 repo = git.Repo(repo_path)
 
+print("Retrieving git info ...")
+
 of_interest = repo.git.log("--grep", "breaking_change_report", "--pretty=format:%H")
+
+print("Processing git info ...")
 
 individual_commit_hashes = of_interest.split('\n')
 
@@ -46,21 +109,26 @@ for hash in breaking_changes_info_raw:
     # print(hash)
     # print(breaking_changes_info_raw[hash]['affected_file_path'])
     
-    first_level_path, _, remaining = breaking_changes_info_raw[hash]['affected_file_path'].partition('/')
-    
-    if len(first_level_path) == 0:
-        first_level_path, _, remaining = remaining.partition('/')
+    for change in breaking_changes_info_raw[hash]:
+        affected_file_path = change['affected_file_path']
+        print(affected_file_path)
+        first_level_path, _, remaining = affected_file_path.partition('/')
         
-    if len(remaining) == 0:
-        first_level_path = 'other'
+        if len(first_level_path) == 0:
+            first_level_path, _, remaining = remaining.partition('/')
+            
+        if len(remaining) == 0:
+            first_level_path = 'other'
+            
+        if not first_level_path in affected_file_paths_and_hashes:
+            affected_file_paths_and_hashes[first_level_path] = {}
         
-    if not first_level_path in affected_file_paths_and_hashes:
-        affected_file_paths_and_hashes[first_level_path] = {}
-    
-    if breaking_changes_info_raw[hash]['affected_file_path'] in affected_file_paths_and_hashes[first_level_path]:
-        affected_file_paths_and_hashes[first_level_path][breaking_changes_info_raw[hash]['affected_file_path']].append(hash)
-    else:
-        affected_file_paths_and_hashes[first_level_path][breaking_changes_info_raw[hash]['affected_file_path']] = [hash]
+        if affected_file_path in affected_file_paths_and_hashes[first_level_path]:
+            affected_file_paths_and_hashes[first_level_path][affected_file_path].append(hash)
+        else:
+            affected_file_paths_and_hashes[first_level_path][affected_file_path] = [hash]
+
+print("Generating output ...")
 
 header_fh = open(template_path + '/pretty-breaking-changes_header.html', 'r')
 entire_header = header_fh.read()
@@ -93,26 +161,25 @@ for first_level_path in affected_file_paths_and_hashes:
         '''
         
         for hash in affected_file_paths_and_hashes[first_level_path][affected_file_path]:
-            info = breaking_changes_info_raw[hash]
-        
-            this_block += '''
-                <li class="list-group-item mb-3">
-                    <div class="what-section">
-                        <div class="what-section-title">What has changed?</div>
-                        <small>{what_info}</small>
-                    </div>
-                    <div class="why-section">
-                        <div class="why-section-title">Why has it changed?</div>
-                        <small>{why_info}</small>
-                    </div>
-                    <div class="see-more-section">
-                        <div class="see-more-section-title">Where can I find more?</div>
-                        <small>
-                            See <a href="https://liferay.atlassian.net/browse/{jira_ticket}">{jira_ticket}</a> {jira_ticket_title}
-                        </small>
-                    </div>
-                </li>
-            '''.format(**info)
+            for change in breaking_changes_info_raw[hash]:
+                this_block += '''
+                    <li class="list-group-item mb-3">
+                        <div class="what-section">
+                            <div class="what-section-title">What has changed?</div>
+                            <small>{what_info}</small>
+                        </div>
+                        <div class="why-section">
+                            <div class="why-section-title">Why has it changed?</div>
+                            <small>{why_info}</small>
+                        </div>
+                        <div class="see-more-section">
+                            <div class="see-more-section-title">Where can I find more?</div>
+                            <small>
+                                See <a href="https://liferay.atlassian.net/browse/{jira_ticket}">{jira_ticket}</a> {jira_ticket_title}
+                            </small>
+                        </div>
+                    </li>
+                '''.format(**change)
 
             
         this_block += '''
