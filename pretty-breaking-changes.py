4e478ace58ffc684a8c5dd03349a05ce42abc6c5
yo <alberto.montero@liferay.com>
2023-11-27 15:32:29 +0100

    Completed processing of commit messages related to multiple files

--- pretty-breaking-changes.py
+++ pretty-breaking-changes.py
@@ -52,9 +52,6 @@ def dissect_commit_message(raw_commit_message):
                 breaking_changes_info.append(info)
                 breaking_changes_info[-1]['jira_ticket'] = jira_ticket
                 breaking_changes_info[-1]['jira_ticket_title'] = jira_ticket_title
-            else:
-                print("unprocessable")
-                print(lines[previous_line_index:next_line_index])
                 
         previous_line_index = next_line_index + 1
         
@@ -69,14 +66,13 @@ def extract_breaking_change_info(lines):
         what_line_index = get_end_of_block(lines, 1, "## What")
         raw_what_header = lines[what_line_index]
         _, _, breaking_change_info['affected_file_path'] = raw_what_header.partition('## What ')
-        print(breaking_change_info['affected_file_path'])
         
         why_line_index = get_end_of_block(lines, what_line_index + 1, "## Why")
-
+        
         breaking_change_info['what_info'] = "\n".join(lines[what_line_index + 1:why_line_index])
-
+        
         alternatives_line_index = get_end_of_block(lines, why_line_index + 1, "## Alternatives")
-
+        
         breaking_change_info['why_info'] = "\n".join(lines[why_line_index + 1: alternatives_line_index])
         
         if len(lines) >= alternatives_line_index:
@@ -84,6 +80,8 @@ def extract_breaking_change_info(lines):
                     
     except Exception as e:
         print(e)
+        print("unprocessable")
+        print(lines)
         # print(raw_commit_message)
         # print("--")
         return None
@@ -111,7 +109,7 @@ for hash in breaking_changes_info_raw:
     
     for change in breaking_changes_info_raw[hash]:
         affected_file_path = change['affected_file_path']
-        print(affected_file_path)
+        # print(affected_file_path)
         first_level_path, _, remaining = affected_file_path.partition('/')
         
         if len(first_level_path) == 0:
@@ -124,9 +122,9 @@ for hash in breaking_changes_info_raw:
             affected_file_paths_and_hashes[first_level_path] = {}
         
         if affected_file_path in affected_file_paths_and_hashes[first_level_path]:
-            affected_file_paths_and_hashes[first_level_path][affected_file_path].append(hash)
+            affected_file_paths_and_hashes[first_level_path][affected_file_path].append(change)
         else:
-            affected_file_paths_and_hashes[first_level_path][affected_file_path] = [hash]
+            affected_file_paths_and_hashes[first_level_path][affected_file_path] = [change]
 
 print("Generating output ...")
 
@@ -160,26 +158,25 @@ for first_level_path in affected_file_paths_and_hashes:
                 <ul>
         '''
         
-        for hash in affected_file_paths_and_hashes[first_level_path][affected_file_path]:
-            for change in breaking_changes_info_raw[hash]:
-                this_block += '''
-                    <li class="list-group-item mb-3">
-                        <div class="what-section">
-                            <div class="what-section-title">What has changed?</div>
-                            <small>{what_info}</small>
-                        </div>
-                        <div class="why-section">
-                            <div class="why-section-title">Why has it changed?</div>
-                            <small>{why_info}</small>
-                        </div>
-                        <div class="see-more-section">
-                            <div class="see-more-section-title">Where can I find more?</div>
-                            <small>
-                                See <a href="https://liferay.atlassian.net/browse/{jira_ticket}">{jira_ticket}</a> {jira_ticket_title}
-                            </small>
-                        </div>
-                    </li>
-                '''.format(**change)
+        for change in affected_file_paths_and_hashes[first_level_path][affected_file_path]:
+            this_block += '''
+                <li class="list-group-item mb-3">
+                    <div class="what-section">
+                        <div class="what-section-title">What has changed?</div>
+                        <small>{what_info}</small>
+                    </div>
+                    <div class="why-section">
+                        <div class="why-section-title">Why has it changed?</div>
+                        <small>{why_info}</small>
+                    </div>
+                    <div class="see-more-section">
+                        <div class="see-more-section-title">Where can I find more?</div>
+                        <small>
+                            See <a href="https://liferay.atlassian.net/browse/{jira_ticket}">{jira_ticket}</a> {jira_ticket_title}
+                        </small>
+                    </div>
+                </li>
+            '''.format(**change)
 
             
         this_block += '''
